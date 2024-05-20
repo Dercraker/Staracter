948a21cfd43688585eca146ace194676ea188042
Dercraker <antoine.capitain@gmail.com>
2024-05-20 17:49:38 +0200

    Add Card Loader

--- src/components/characters/characterList.tsx
+++ src/components/characters/characterList.tsx
@@ -1,5 +1,7 @@
 import type { GetPaginatedCharactersQuery } from '@/features/character/getPaginatedCharacters.query';
 import { Group, Stack } from '@mantine/core';
+import { Suspense } from 'react';
+import { CharacterCardLoader } from '../loader/characterCard.loader';
 import { Character3DCard } from './character3DCard';
 
 export type CharacterListProps = {
@@ -11,12 +13,9 @@ export const CharacterList = ({ characters }: CharacterListProps) => {
     <Stack>
       <Group justify="space-evenly" align="center">
         {characters.map((character, idx) => (
-          <>
-            {/* <CharacterCard key={idx} {...character}>
-              <CharacterCardSection {...character} />
-            </CharacterCard> */}
-            <Character3DCard key={`3d-${idx}`} {...character} />
-          </>
+          <Suspense key={idx} fallback={<CharacterCardLoader />}>
+            <Character3DCard {...character} />
+          </Suspense>
         ))}
       </Group>
     </Stack>
